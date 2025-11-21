bbd3da282433d524ae5e718ecfee968352561bb4
Shikhar Rai <tonyrai426@gmail.com>
2025-11-22 02:48:11 +0530

    Backend Changes

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from routes.main_routes import main_bp
+from db import db, init_db
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(Config)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # Bind DB to app
+    db.init_app(app)
+
+    CORS(app, supports_credentials=True)
     app.register_blueprint(main_bp)
+
+    with app.app_context():
+        init_db()
 
     return app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
