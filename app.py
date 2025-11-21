2400c192a5d57f5cb0ec1a82fb5a1d379f3ca891
Shikhar Rai <tonyrai426@gmail.com>
2025-11-21 20:46:28 +0530

    Changes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from routes.main_routes import main_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(main_bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000)
